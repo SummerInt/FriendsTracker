998e236e65df0a19b231336cf45c2aa1edd575e4
Maria <vasilevamariav@yandex.ru>
2018-06-30 22:40:05 +0400

    copy from google map template

--- app/src/main/java/com/teamm/friendstracker/ui/MapActivity.java
+++ app/src/main/java/com/teamm/friendstracker/ui/MapActivity.java
@@ -1,6 +1,6 @@
 package com.teamm.friendstracker.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -8,12 +8,23 @@ import android.widget.ListView;
 import com.teamm.friendstracker.R;
 import com.teamm.friendstracker.view.MapView;
 
-public class MapActivity extends AppCompatActivity implements MapView {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements MapView, OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         //DrawerLayout navigationLayout = findViewById(R.id.navigation_layout);
         ListView navigationListView = findViewById(R.id.navigation_list_view);
@@ -22,5 +33,15 @@ public class MapActivity extends AppCompatActivity implements MapView {
         navigationListView.setAdapter(new ArrayAdapter<>(this,
                 R.layout.navigation_list_item, navigationTitles));
         //navigationListView.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
