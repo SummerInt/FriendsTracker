afcb09647608c3683e059d969d6f92f1a89d31ca
Kate <avisfy@mail.ru>
2018-06-30 16:41:30 +0400

    Main activity:menu (friends, exit). Make exit as exit on Login activity/

--- app/src/main/java/com/teamm/friendstracker/ui/MainActivity.java
+++ app/src/main/java/com/teamm/friendstracker/ui/MainActivity.java
@@ -78,20 +78,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_friends) {
+            // intenr friend
+        } else if (id == R.id.nav_exit) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
