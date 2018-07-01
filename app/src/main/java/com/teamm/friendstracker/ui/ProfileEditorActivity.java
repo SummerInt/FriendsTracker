c2f81b23587995f867732477114aedf8397b1b8a
Evgeniy Bashmakov <suplasma@ya.ru>
2018-07-02 01:09:01 +0400

    update

--- app/src/main/java/com/teamm/friendstracker/ui/ProfileEditorActivity.java
+++ app/src/main/java/com/teamm/friendstracker/ui/ProfileEditorActivity.java
@@ -1,10 +1,12 @@
 package com.teamm.friendstracker.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,19 +28,14 @@ import com.teamm.friendstracker.model.db.DbManager;
 import com.teamm.friendstracker.model.entity.User;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProfileEditorActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button bChangePhoto;
-    Button bAccept;
-    Button bExit;
-    Uri selectedImage = null;
-    EditText name;
-    EditText surname;
-    private User user = DbManager.user;
-    private static final int SELECT_IMAGE = 1 ;
+    EditText name, surname;
+    private static final int SELECT_IMAGE = 123 ;
     ImageView image;
     public static boolean photoChanged = false;
     FirebaseStorage storage = FirebaseStorage.getInstance();
@@ -49,20 +46,15 @@ public class ProfileEditorActivity extends AppCompatActivity implements View.OnC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_editor);
-        bChangePhoto = (Button) findViewById(R.id.bChangePhoto);
-        bAccept = (Button) findViewById(R.id.bAccept);
-        bExit = (Button) findViewById(R.id.bExit);
+
         name=(EditText)findViewById(R.id.etNewName);
         surname=(EditText)findViewById(R.id.etNewSurname);
-        image=(ImageView) findViewById(R.id.imageView2);
-
-        bChangePhoto.setOnClickListener(this);
-        bAccept.setOnClickListener(this);
-        bExit.setOnClickListener(this);
+        image=(ImageView) findViewById(R.id.imageV);
 
         name.setText(DbManager.user.getName());
         surname.setText(DbManager.user.getSurname());
-        avatarDownload();
+        if(DbManager.user.getAvatar())
+            avatarDownload();
 
         name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -125,60 +117,60 @@ public class ProfileEditorActivity extends AppCompatActivity implements View.OnC
         return matcher.find();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.bChangePhoto:
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(Intent.createChooser(photoPickerIntent, "Выберите изображение"), SELECT_IMAGE);
-                break;
-            case R.id.bAccept:
-                String newName = name.getText().toString();
-                String newSurname = surname.getText().toString();
-                String newPhoto;
-                DbManager.user.setName(newName);
-                DbManager.user.setName(newSurname);
-                /*if(!newName.isEmpty()){
-                    user.setName(newName);
-                }
-                if(!newSurname.isEmpty()){
-                    user.setSurname(newSurname);
-                }*/
-                if(selectedImage != null){
-                    newPhoto = selectedImage.toString();
-                }else {
-                    newPhoto = "";
-                }
-                Intent intent = new Intent(this,MainActivity.class);
-                intent.putExtra("name", newName);
-                intent.putExtra("surname", newSurname);
-                intent.putExtra("photo", newPhoto);
-                setResult(RESULT_OK, intent);
-                if(photoChanged){
-                    DbManager.user.setAvatar(true);
-                    avatarSave();
-                }
-                DbManager.write();
-                finish();
-                break;
-            case R.id.bExit:
-                DbManager.signOut();
-                Intent intentExit = new Intent(ProfileEditorActivity.this, LoginActivity.class);
-                startActivity(intentExit);
-                break;
+    public void onClick(View view){
+        switch (view.getId()){
 
+            case R.id.bChangePhoto: {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);//
+                startActivityForResult(Intent.createChooser(intent, "Выберите изображение"), SELECT_IMAGE);
+                break;
+            }
+
+            case R.id.bAccept: {
+                if(isRightName(name.getText().toString()))
+                    if(isRightSurname(surname.getText().toString())) {
+                        DbManager.user.setName(name.getText().toString());
+                        DbManager.user.setSurname(surname.getText().toString());
+                        if (photoChanged) {
+                            DbManager.user.setAvatar(true);
+                            avatarSave();
+                        }
+                        DbManager.write();
+                        Toast.makeText(getApplicationContext(), "Сохранено", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                        isRightSurname(surname.getText().toString());
+                break;
+            }
+
+            case R.id.bExit: {
+                DbManager.signOut();
+                Intent intent = new Intent(ProfileEditorActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SELECT_IMAGE) {
-            if (resultCode == RESULT_OK) {
-                selectedImage = data.getData();
-                image.setImageURI(null);
-                image.setImageURI(selectedImage);
-                photoChanged = true;
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    try {
+                        Bitmap bitmap;
+                        image.setImageBitmap(bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData()));
+                        photoChanged = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Отмена", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -208,9 +200,9 @@ public class ProfileEditorActivity extends AppCompatActivity implements View.OnC
 
     public void avatarDownload(){
         StorageReference storageReference = storageRef.child(DbManager.users.getUid()).child("avatar.jpg");
-        Glide.with(this /* context */)
+        Glide.with(ProfileEditorActivity.this)
                 .using(new FirebaseImageLoader())
-                .load(mountainsRef)
+                .load(storageReference)
                 .into(image);
     }
 }
