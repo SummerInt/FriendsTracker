d84a544a44ae6a744d5858cd2383764981c1a774
Kate <avisfy@mail.ru>
2018-07-02 02:20:51 +0400

    add search friend activity

---
+++ app/src/main/java/com/teamm/friendstracker/present/RVSearchAdapter.java
@@ -0,0 +1,74 @@
+package com.teamm.friendstracker.present;
+import com.teamm.friendstracker.R;
+import com.teamm.friendstracker.model.entity.User;
+
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class RVSearchAdapter extends RecyclerView.Adapter<RVSearchAdapter.PersonViewHolder>{
+
+    private List<User> friends =  new ArrayList<>();
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.friend_search_card, viewGroup, false);
+        return new PersonViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        personViewHolder.tvName.setText(friends.get(i).getName()+" "+friends.get(i).getSurname());
+        personViewHolder.tvMail.setText(friends.get(i).getEmail());
+        //personViewHolder.friendPhoto.setImageResource(friends.get(i).getPhoto());
+    }
+
+    public void clearItems() {
+        friends.clear();
+        notifyDataSetChanged();
+    }
+
+    public void setItems(Collection<User> newFriends) {
+        friends.addAll(newFriends);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return friends.size();
+    }
+
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView tvName;
+        TextView tvMail;
+        Button bAdd;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cvFriend);
+            tvName = (TextView)itemView.findViewById(R.id.tvName);
+            tvMail = (TextView)itemView.findViewById(R.id.tvMail);
+            bAdd= (Button)itemView.findViewById(R.id.bAdd);
+        }
+    }
+
+}
+
