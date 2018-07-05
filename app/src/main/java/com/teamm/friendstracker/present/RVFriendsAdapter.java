92b13c087dd5cc4c351821d8b512e745ba36c2fb
Kate <avisfy@mail.ru>
2018-07-05 20:13:19 +0400

    add menu "operations with friends"

--- app/src/main/java/com/teamm/friendstracker/present/RVFriendsAdapter.java
+++ app/src/main/java/com/teamm/friendstracker/present/RVFriendsAdapter.java
@@ -1,14 +1,19 @@
 package com.teamm.friendstracker.present;
 import com.teamm.friendstracker.R;
 import com.teamm.friendstracker.model.entity.User;
+import com.teamm.friendstracker.ui.FriendActivity;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,6 +23,16 @@ public class RVFriendsAdapter extends RecyclerView.Adapter<RVFriendsAdapter.Pers
 
     private ArrayList<User> friends =  new ArrayList<>();
 
+    public interface OnCardClickListener {
+        void onCardClick(View view,  int position);
+    }
+
+    public void setOnCardClickListener(OnCardClickListener listener) {
+        mListener = listener;
+    }
+
+    private static OnCardClickListener mListener;
+
     @Override
     public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.friend_card, viewGroup, false);
@@ -26,10 +41,12 @@ public class RVFriendsAdapter extends RecyclerView.Adapter<RVFriendsAdapter.Pers
 
     @Override
     public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        final int index = i;
         personViewHolder.friendName.setText(friends.get(i).getName()+" "+friends.get(i).getSurname());
         personViewHolder.friendMail.setText(friends.get(i).getEmail());
         //personViewHolder.friendPhoto.setImageResource(friends.get(i).getPhoto());
     }
+
 
     public void clearItems() {
         friends.clear();
@@ -53,11 +70,12 @@ public class RVFriendsAdapter extends RecyclerView.Adapter<RVFriendsAdapter.Pers
         super.onAttachedToRecyclerView(recyclerView);
     }
 
-    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+    public static class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         CardView cv;
         TextView friendName;
         TextView friendMail;
         ImageView friendPhoto;
+        RelativeLayout friendLayout;
 
 
         PersonViewHolder(View itemView) {
@@ -66,6 +84,24 @@ public class RVFriendsAdapter extends RecyclerView.Adapter<RVFriendsAdapter.Pers
             friendName = (TextView)itemView.findViewById(R.id.friend_name);
             friendMail = (TextView)itemView.findViewById(R.id.friend_mail);
             friendPhoto = (ImageView)itemView.findViewById(R.id.friend_photo);
+            friendLayout = (RelativeLayout)itemView.findViewById(R.id.friendLayout);
+            cv.setOnClickListener(this);
+            /*itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    PopupMenu popup = new PopupMenu(view.getContext(), view);
+                    popup.inflate(R.menu.popup_menu_friends);
+                    popup.setOnMenuItemClickListener(PersonViewHolder.this);
+                    popup.show();
+                }
+            });*/
+        }
+
+        @Override
+        public void onClick(View v) {
+            // вызов метода-колбэка и назначение передаваемых значений аргументам
+            int position = getAdapterPosition();
+            mListener.onCardClick(v, position);
         }
     }
 
--- app/src/main/java/com/teamm/friendstracker/ui/FriendActivity.java
+++ app/src/main/java/com/teamm/friendstracker/ui/FriendActivity.java
@@ -10,19 +10,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class FriendActivity extends AppCompatActivity implements View.OnClickListener{
+public class FriendActivity extends AppCompatActivity implements View.OnClickListener, RVFriendsAdapter.OnCardClickListener{
 
     static RecyclerView rvFriends;
 
     static RVFriendsAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +36,37 @@ public class FriendActivity extends AppCompatActivity implements View.OnClickLis
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rvFriends.setLayoutManager(llm);
         adapter = new RVFriendsAdapter();
-
+        adapter.setOnCardClickListener(this);
 
         Button bAddFriend = (Button)findViewById(R.id.bAddFriend);
         bAddFriend.setOnClickListener(this);
     }
+
+    @Override
+    public void onCardClick(View view,final int index) {
+        showPopupMenu(view, index);
+    }
+
+    private void showPopupMenu(View v, int i) {
+        final int index = i;
+        PopupMenu popupMenu = new PopupMenu(FriendActivity.this, v);
+        popupMenu.inflate(R.menu.popup_menu_friends);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete:
+                        //Удаленпие друга тут
+                        Toast.makeText(getApplicationContext(), "Удалено", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
+
 
     @Override
     protected void onResume() {
